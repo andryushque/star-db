0719e4dd7aecdc79d51ff972fd4dad3de1afc9d7
Andrey Ustinov <54977836+andryushque@users.noreply.github.com>
2020-12-11 18:49:42 +0300

    Add: fetch api

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-/*  */
+const getResourse = async (url) => {
+  const res = await fetch(url);
+  const body = await res.json();
+  return body;
+}
+
+const url = 'https://swapi.dev/api/people/1';
+
+getResourse(url)
+  .then((body) => {
+    console.log(body);
+  })
