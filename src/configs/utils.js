4e2374a29954e9f684ca247b4e01d13cf7f01077
Andrey Um <a.um@devs-marketing.ru>
2021-04-02 16:06:09 +0300

    Create: catching errors

--- src/configs/utils.js
+++ src/configs/utils.js
@@ -1,6 +1,11 @@
 module.exports = {
   getResourse: async (url) => {
     const res = await fetch(url);
+
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}, recieved ${res.status}`);
+    }
+
     const body = await res.json();
     return body;
   },
--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ import { baseUrl } from "./configs/config";
 import { getResourse } from "./configs/utils";
 
 // test
-const url = `${baseUrl}api/people/1`;
+const url = `${baseUrl}api/people/155555`;
 
-getResourse(url).then((body) => {
-  console.log("test", body);
-});
+getResourse(url)
+  .then((body) => {
+    console.log("test", body);
+  })
+  .catch((err) => {
+    console.error('Could not fetch!', err);
+  });
